181aa344a87a1bddf3449a6f7f5a71e849f24d14
Robert Roskam <raiderrobert@gmail.com>
2016-09-09 15:59:17 -0400

    Update main.py

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -15,6 +15,8 @@ def main(minutes, seconds):
     seconds = 0 if seconds is None else int(seconds)
     for i in bar(range(minutes*60+seconds)):
         time.sleep(1)
+        
+    print("Take a 5 minutes break")
 
 if __name__ == '__main__':
     main()
