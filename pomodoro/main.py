e73aca80df1492192b5b8ff88e193052940dce55
Robert Roskam <raiderrobert@gmail.com>
2016-09-09 15:56:48 -0400

    Update main.py

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -6,12 +6,14 @@ import progressbar
 import time
 
 @click.command()
-@click.option('--minutes', default=25, help='Number of minutes, default 25.')
-def main(minutes):
+@click.option('--minutes', '-m',  default=25, help='Number of minutes, default 25.')
+@click.option('--seconds', '-s', help='Number of seconds.')
+def main(minutes, seconds):
     bar = progressbar.ProgressBar(widgets=[
         progressbar.Bar(),
     ])
-    for i in bar(range(minutes*60)):
+    seconds = 0 if seconds is None else seconds
+    for i in bar(range(minutes*60+seconds)):
         time.sleep(1)
 
 if __name__ == '__main__':
