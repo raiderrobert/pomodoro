089d56cf1fd2e97ddfab08018f0baea1c347a6ea
Robert Roskam <raiderrobert@gmail.com>
2016-09-09 16:08:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_file(filename):
     except IOError:
         return ''
 
-install_requires = ['click', 'progressbar2']
+install_requires = ['click', 'progressbar2', 'playsound']
 
 setup(
     name=NAME,
